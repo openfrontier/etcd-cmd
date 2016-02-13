135098490768d3503e81af6aa291678875fdfeeb
thinkernel <thinkernel@gmail.com>
2016-02-13 21:09:48 +0800

    Add port 7001 as peer listening port

--- joinCluster.sh
+++ joinCluster.sh
@@ -6,7 +6,7 @@ DISCOVERY_IP=$2
 UUID=$3
 
 etcd2 --name ${HOSTNAME} --data-dir /tmp/etcd \
-  --initial-advertise-peer-urls "http://${HOST_IP}:2380" \
+  --initial-advertise-peer-urls "http://${HOST_IP}:2380,http://${HOST_IP}:7001" \
   --listen-peer-urls            "http://0.0.0.0:2380" \
   --listen-client-urls          "http://0.0.0.0:2379,http://0.0.0.0:4001" \
   --advertise-client-urls       "http://${HOST_IP}:2379,http://${HOST_IP}:4001" \
