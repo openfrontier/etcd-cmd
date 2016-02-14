c6820fe49eb1e9e26e2689264bcb8bf33bd10f8e
thinkernel <thinkernel@gmail.com>
2016-02-14 18:46:28 +0800

    Change output of getClusterToken

--- getClusterToken.sh
+++ getClusterToken.sh
@@ -6,4 +6,4 @@ UUID=$(uuidgen)
 
 curl -X PUT http://${DISCOVERY_IP}:2379/v2/keys/discovery/${UUID}/_config/size -d value=3
 
-echo "-discovery http://${DISCOVERY_IP}:2379/v2/keys/discovery/${UUID}"
+echo "token=${UUID}"
